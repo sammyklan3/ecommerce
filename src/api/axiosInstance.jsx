e20a9dee3e80ae1274838a76cac6b13024732fbf
sammyklan3 <137900210+sammyklan3@users.noreply.github.com>
2024-02-12 18:51:33 +0300

    Update axiosInstance.jsx

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -1,7 +1,7 @@
 // axiosInstance.js
 import axios from 'axios';
 
-const BASE_URL = 'http://ecommerce-backend-xkso.onrender.com:3000/';
+const BASE_URL = 'https://ecommerce-backend-xkso.onrender.com:3000/';
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
