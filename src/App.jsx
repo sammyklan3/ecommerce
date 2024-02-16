8d8e51d55ff93eb7a5fc575071adb00698bbc41c
sammyklan3 <sammyklane7@gmail.com>
2024-02-16 21:04:02 +0300

    react-router update

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
-import { Home } from "./pages/home/Home";
-import { Checkout } from "./pages/checkout/Checkout";
-import { Cart } from "./pages/cart/Cart";
-import { NoPage } from './pages/nopage/NoPage';
-import { Login } from './pages/login/Login';
-import { Admin } from "./admin/pages/home/Admin";
-import { Signup } from './pages/signup/Signup';
-import { Account } from "./pages/Account/Account";
-import { ProductCreation } from './admin/productCreation/ProductCreation';
-import { Products } from "./admin/pages/products/Products";
-import { ProductDetail } from "./pages/product/product";
-
 import './App.css'
 
 function App() {
 
   return (
     <>
-    
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:ProductID" element={<ProductDetail/>} />
-        <Route path="/adminDashboard" element={<Admin />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/admin/createproduct" element={< ProductCreation />} />
-        <Route path="/admin/products" element={< Products />} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,77 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Home } from "./pages/home/Home";
+import { Checkout } from "./pages/checkout/Checkout";
+import { Cart } from "./pages/cart/Cart";
+import { NoPage } from './pages/nopage/NoPage';
+import { Login } from './pages/login/Login';
+import { Signup } from "./pages/signup/Signup";
+import { Admin } from "./admin/pages/home/Admin";
+import { Account } from "./pages/Account/Account";
+import { ProductCreation } from './admin/productCreation/ProductCreation';
+import { Products } from "./admin/pages/products/Products";
+import { ProductDetail } from "./pages/product/product";
 import './index.css';
 import { CartProvider } from './context/Cart.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/product/:ProductID",
+    element: <ProductDetail />,
+  },
+  {
+    path: "/adminDashboard",
+    element: <Admin />,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/account",
+    element: <Account />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/admin/createproduct",
+    element: < ProductCreation />,
+  },
+  {
+    path: "/admin/products",
+    element: < Products />,
+  },
+  {
+    path: "*",
+    element: <NoPage />,
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
       <AuthProvider>
         <CartProvider>
-          <App />
+          <RouterProvider router={router} />
         </CartProvider>
       </AuthProvider>
-    </BrowserRouter>
   </React.StrictMode>,
 )
